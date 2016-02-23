83f0629a1df70d8ea53f0e57dda7dca65f0670fc
Eduardo Alvarado <e.alvaradod@gmail.com>
2016-02-23 00:24:50 -0600

    first commit

---
+++ ParseStarterProject/SignUpViewController.swift
@@ -0,0 +1,94 @@
+//
+//  SignUpViewController.swift
+//  ParseStarterProject
+//
+//  Created by Rob Percival on 13/07/2015.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func signUp(sender: AnyObject) {
+        
+        PFUser.currentUser()?["interestedInWomen"] = interestedInWomen.on
+        PFUser.currentUser()?.save()
+        
+        self.performSegueWithIdentifier("logUserIn", sender:self)
+        
+    }
+    
+    
+    @IBOutlet var interestedInWomen: UISwitch!
+    @IBOutlet var userImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+               
+
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, gender, email"])
+        graphRequest.startWithCompletionHandler( {
+          
+            (connection, result, error) -> Void in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else if let result = result {
+                
+                print(result)
+                
+                PFUser.currentUser()?["gender"] = result["gender"]
+                PFUser.currentUser()?["name"] = result["name"]
+                PFUser.currentUser()?["email"] = result["email"]
+                
+                PFUser.currentUser()?.save()
+                
+                let userId = result["id"] as! String
+                
+                let facebookProfilePictureUrl = "https://graph.facebook.com/" + userId + "/picture?type=large"
+                
+                if let fbpicUrl = NSURL(string: facebookProfilePictureUrl) {
+                    
+                    if let data = NSData(contentsOfURL: fbpicUrl) {
+                        
+                        self.userImage.image = UIImage(data: data)
+                        
+                        let imageFile:PFFile = PFFile(data: data)
+                        
+                        PFUser.currentUser()?["image"] = imageFile
+                        
+                        PFUser.currentUser()?.save()
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        })
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
